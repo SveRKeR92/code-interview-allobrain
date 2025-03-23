445efb967deadf135871ba365e0d10a9ca8c4cf8
Sverker <sverker.dev@gmail.com>
2025-03-23 17:15:48 +0100

    props definition with interface for NoteCard

--- frontend/src/components/NoteCard.tsx
+++ frontend/src/components/NoteCard.tsx
@@ -1,11 +1,15 @@
 import {Note} from "../types/note.ts";
 import '../styles/NoteCard.css'
 
-function NoteCard({ note }: { note: Note }) {
+interface NoteCardProps {
+  note: Note;
+}
+
+function NoteCard(props: NoteCardProps) {
   return(
     <>
       <div className="note-card">
-        <h3>{note.title}</h3>
+        <h3>{props.note.title}</h3>
       </div>
     </>
   )
